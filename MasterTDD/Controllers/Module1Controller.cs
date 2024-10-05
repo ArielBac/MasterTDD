f3b88ba5f7862c6b780a37d1d668a936d22b7105
ArielBac <arielvieira65@gmail.com>
2024-10-05 15:42:26 -0400

    Call Leap Year service in the Module1 controller

--- MasterTDD/Controllers/Module1Controller.cs
+++ MasterTDD/Controllers/Module1Controller.cs
@@ -1,15 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MasterTDD.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MasterTDD.Controllers
 {
     [Route("api/module-1")]
+    [Produces("application/json")]
     [ApiController]
     public class Module1Controller : ControllerBase
     {
-        [HttpGet("leap-year")]
+        [HttpGet("leap-year/{year}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult GetLeapYear([FromRoute] int year)
         {
-            return Ok();
+            var isLeapYear = LeapYearService.Verify(year);
+
+            if (isLeapYear)
+                return Ok("Is Leap Year");
+
+            return Ok("Is not Leap Year");
         }
     }
 }
