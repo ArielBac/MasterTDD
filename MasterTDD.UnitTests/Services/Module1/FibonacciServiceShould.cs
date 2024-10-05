6265abdd5d59652cf37fcf2dfe26038f6ea98642
ArielBac <arielvieira65@gmail.com>
2024-10-05 15:57:25 -0400

    Adding second test

--- MasterTDD.UnitTests/Services/Module1/FibonacciServiceShould.cs
+++ MasterTDD.UnitTests/Services/Module1/FibonacciServiceShould.cs
@@ -6,11 +6,19 @@ namespace MasterTDD.UnitTests.Services.Module1
     public class FibonacciServiceShould
     {
         [Fact]
-        public void Return1_AtPosition1()
+        public void Return0_AtPosition1()
         {
             var fibonacciNumber = FibonacciService.GetNumberAtPosition(1);
 
             fibonacciNumber.Should().Be(0);
+        }
+
+        [Fact]
+        public void Return1_AtPosition2()
+        {
+            var fibonacciNumber = FibonacciService.GetNumberAtPosition(2);
+
+            fibonacciNumber.Should().Be(1);
         }
     }
 }
