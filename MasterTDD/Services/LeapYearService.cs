f9ea96af43f9f6fb934aa4164ebca0b4c859fb57
ArielBac <arielvieira65@gmail.com>
2024-10-05 15:13:50 -0400

    Implementing production code logic

--- MasterTDD/Services/LeapYearService.cs
+++ MasterTDD/Services/LeapYearService.cs
@@ -4,10 +4,7 @@
     {
         public static bool Verify(int year)
         {
-            if (year == 400 || year == 500 || year == 600)
-                return false;
-
-            if (year % 4 == 0)
+            if (year % 4 == 0 && year % 100 != 0)
                 return true;
 
             return false;
