dd4defbe0cce42f02c4e2b8a393f3041b8d2bdd7
ArielBac <arielvieira65@gmail.com>
2024-10-05 17:36:07 -0400

    Implementing prodution code to pass in the tests

--- MasterTDD/Services/Module1/FibonacciService.cs
+++ MasterTDD/Services/Module1/FibonacciService.cs
@@ -7,22 +7,7 @@
             if (position < 2)
                 return position;
 
-            int aux1 = 1;
-            int aux2 = 1;
-            int fibonacciNumber = 1;
-
-            for (int i = 2; i < position; i++)
-            {
-                fibonacciNumber = aux1 + aux2;
-
-                if (i == position - 2)
-                    aux1 = fibonacciNumber;
-
-                if (i == position - 1)
-                    aux2 = fibonacciNumber;
-            }
-
-            return fibonacciNumber;
+            return GetNumberAtPosition(position - 2) + GetNumberAtPosition(position - 1);
         }
     }
 }
