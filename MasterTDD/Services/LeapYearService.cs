7c8abd0cee7cdd3516de11e39fd8a90255b90f53
ArielBac <arielvieira65@gmail.com>
2024-10-05 15:32:06 -0400

    Implementing the logic

--- MasterTDD/Services/LeapYearService.cs
+++ MasterTDD/Services/LeapYearService.cs
@@ -4,13 +4,7 @@
     {
         public static bool Verify(int year)
         {
-            if (year == 400 || year == 800 || year == 1200)
-                return true;
-
-            if (year % 4 == 0 && year % 100 != 0)
-                return true;
-
-            return false;
+            return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
         }
     }
 }
