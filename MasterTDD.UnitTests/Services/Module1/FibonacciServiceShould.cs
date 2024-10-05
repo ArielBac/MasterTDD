70e7e80d52919cfe6fcfd8e249ef2bffed230302
ArielBac <arielvieira65@gmail.com>
2024-10-05 15:56:04 -0400

    Adding fibonacci fisrt test

---
+++ MasterTDD.UnitTests/Services/Module1/FibonacciServiceShould.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using MasterTDD.Services.Module1;
+
+namespace MasterTDD.UnitTests.Services.Module1
+{
+    public class FibonacciServiceShould
+    {
+        [Fact]
+        public void Return1_AtPosition1()
+        {
+            var fibonacciNumber = FibonacciService.GetNumberAtPosition(1);
+
+            fibonacciNumber.Should().Be(0);
+        }
+    }
+}
---
+++ MasterTDD/Services/Module1/FibonacciService.cs
@@ -0,0 +1,10 @@
+﻿namespace MasterTDD.Services.Module1
+{
+    public static class FibonacciService
+    {
+        public static int GetNumberAtPosition(int position)
+        {
+            return default;
+        }
+    }
+}
