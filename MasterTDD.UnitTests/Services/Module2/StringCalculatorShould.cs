79c5f37454b4d33664c72a8ed8263615eb72bbc8
ArielBac <arielvieira65@gmail.com>
2024-10-09 17:32:33 -0400

    Add StringCaculator base and first failing test

---
+++ MasterTDD.UnitTests/Services/Module2/StringCalculatorShould.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using MasterTDD.Services.Module2;
+
+namespace MasterTDD.UnitTests.Services.Module2
+{
+    public class StringCalculatorShould
+    {
+        [Fact]
+        public void ReturnZeroWhenEmptyString()
+        {
+            string numbers = string.Empty;
+
+            var result = StringCalculatorService.Add(numbers);
+
+            result.Should().Be(0);
+        }
+    }
+}
---
+++ MasterTDD/Services/Module2/StringCalculatorService.cs
@@ -0,0 +1,10 @@
+﻿namespace MasterTDD.Services.Module2
+{
+    public static class StringCalculatorService
+    {
+        public static int Add(string numbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
