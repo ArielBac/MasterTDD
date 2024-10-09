389afbba9e1a1f89a7f32364ca7bc4162a4836c0
ArielBac <arielvieira65@gmail.com>
2024-10-09 17:48:21 -0400

    Implement the StringCaculator production code logic

--- MasterTDD/Services/Module2/StringCalculatorService.cs
+++ MasterTDD/Services/Module2/StringCalculatorService.cs
@@ -4,16 +4,10 @@
     {
         public static int Add(string numbers)
         {
-            if (numbers == "0")
+            if (numbers == string.Empty)
                 return 0;
 
-            if (numbers == "1")
-                return 1;
-
-            if (numbers == "2")
-                return 2;
-
-            return 0;
+            return int.Parse(numbers);
         }
     }
 }
