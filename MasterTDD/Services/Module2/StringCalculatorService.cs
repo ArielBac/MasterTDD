3db7921414d0b234c71e22459821080aed410311
ArielBac <arielvieira65@gmail.com>
2024-10-09 18:20:34 -0400

    Implement the StringCaculator production code logic

--- MasterTDD/Services/Module2/StringCalculatorService.cs
+++ MasterTDD/Services/Module2/StringCalculatorService.cs
@@ -7,20 +7,16 @@
             if (numbers == string.Empty)
                 return 0;
 
-            if (numbers == "0,1,2")
-                return 3;
-
-            if (numbers == "0,1,2,3")
-                return 6;
-
             var numbersArray = numbers.Split(',');
 
-            if (numbersArray.Length == 2)
+            if (numbersArray.Length >= 2)
             {
-                var number1 = int.Parse(numbersArray[0]);
-                var number2 = int.Parse(numbersArray[1]);
+                var sum = 0;
 
-                return number1 + number2;
+                foreach (var number in numbersArray)
+                    sum += int.Parse(number);
+
+                return sum;
             }
 
             return int.Parse(numbers);
