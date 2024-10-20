161b834e52bd2e20d0a16384c045bdf7bde14845
ArielBac <arielvieira65@gmail.com>
2024-10-20 09:08:21 -0400

    Refactor the ChangeCalculator production code

--- MasterTDD/Services/Module3/ChangeCalculatorService.cs
+++ MasterTDD/Services/Module3/ChangeCalculatorService.cs
@@ -4,14 +4,14 @@
     {
         public static double[] GetChange(double totalCost, double totalPaid)
         {
-            var totalChange = totalPaid - totalCost;
             var validDenominations = new[] { 100, 50, 20, 10, 5, 1, 0.5, 0.25, 0.1, 0.05, 0.01 };
+            var totalChange = totalPaid - totalCost;
             var denominations = new List<double>() { };
-
 
             for (var i = 0; i < validDenominations.Length; i++)
             {
-                if (Math.Round(totalChange / validDenominations[i], 2, MidpointRounding.AwayFromZero) >= 1)
+                var division = Math.Round(totalChange / validDenominations[i], 3, MidpointRounding.ToPositiveInfinity);
+                if (division >= 1)
                 {
                     denominations.Add(validDenominations[i]);
                     totalChange -= validDenominations[i];
