c42fb4bbbf42754368712dc474799ef8d0b1c475
ArielBac <arielvieira65@gmail.com>
2024-10-26 10:20:32 -0400

    Add two more scenarios to the BowlingGame tests and make them pass

--- MasterTDD.UnitTests/Services/Module4/BowlingGameServiceShould.cs
+++ MasterTDD.UnitTests/Services/Module4/BowlingGameServiceShould.cs
@@ -8,6 +8,8 @@ namespace MasterTDD.UnitTests.Services.Module4
         [Theory]
         [InlineData("X|X|X|X|X|X|X|X|X|X||XX", 300)]
         [InlineData("9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||", 90)]
+        [InlineData("8-|8-|8-|8-|8-|8-|8-|8-|8-|8-||", 80)]
+        [InlineData("5-|5-|5-|5-|6-|6-|7-|7-|7-|7-||", 60)]
         public void ReturnTotalScore(string input, int expectedScore)
         {
             var score = BowlingGameService.Run(input);
