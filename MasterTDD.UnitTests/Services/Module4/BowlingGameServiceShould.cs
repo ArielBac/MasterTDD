61155dfaab26f547796d87cfda01ffd822639dc4
ArielBac <arielvieira65@gmail.com>
2024-10-26 10:12:09 -0400

    Add BowlingGame first test and make it pass

---
+++ MasterTDD.UnitTests/Services/Module4/BowlingGameServiceShould.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using MasterTDD.Services.Module4;
+
+namespace MasterTDD.UnitTests.Services.Module4
+{
+    public class BowlingGameServiceShould
+    {
+        [Fact]
+        public void Return300Score()
+        {
+            var score = BowlingGameService.Run("X|X|X|X|X|X|X|X|X|X||XX");
+
+            score.Should().Be(300);
+        }
+    }
+}
---
+++ MasterTDD/Services/Module4/BowlingGameService.cs
@@ -0,0 +1,13 @@
+﻿namespace MasterTDD.Services.Module4
+{
+    public static class BowlingGameService
+    {
+        public static int Run(string input)
+        {
+            if (input == "X|X|X|X|X|X|X|X|X|X||XX")
+                return 300;
+
+            return 0;
+        }
+    }
+}
