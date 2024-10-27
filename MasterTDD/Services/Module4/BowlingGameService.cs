fcefddc0f26441f65134dc385d27df96b6bd6cc8
ArielBac <arielvieira65@gmail.com>
2024-10-27 08:58:54 -0400

    Implement the BowlingGame production code logic

--- MasterTDD/Services/Module4/BowlingGameService.cs
+++ MasterTDD/Services/Module4/BowlingGameService.cs
@@ -7,24 +7,15 @@
             if (input == "X|X|X|X|X|X|X|X|X|X||XX")
                 return 300;
 
-            if (input == "-9|-9|-9|-9|-9|-9|-9|-9|-9|-9||")
-                return 90;
-
-            if (input == "-8|-8|-8|-8|-8|-8|-8|-8|-8|-8||")
-                return 80;
-
-            if (input == "-5|-5|-5|-5|-6|-6|-7|-7|-7|-7||")
-                return 60;
-
             var frames = input.Split('|');
             var totalScore = 0;
             foreach (var frame in frames)
             {
+                if (frame.Contains('-'))
+                    totalScore += int.Parse(frame.First(x => x != '-').ToString());
+
                 if (frame.Length > 1)
                 {
-                    if (frame[1] == '-')
-                        totalScore += int.Parse(frame[0].ToString());
-
                     if (frame[1] == '/')
                         totalScore += 10 + int.Parse(frame[0].ToString());
                 }
