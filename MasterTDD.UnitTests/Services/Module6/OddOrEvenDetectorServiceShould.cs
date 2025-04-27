71a2b2a436032fb6cdb0fca6bf43215cf60da4cd
ArielBac <arielvieira65@gmail.com>
2025-04-27 11:24:21 -0400

    Implementing OddOrEvenDetector first test case

--- MasterTDD.UnitTests/Services/Module6/OddOrEvenDetectorServiceShould.cs
+++ MasterTDD.UnitTests/Services/Module6/OddOrEvenDetectorServiceShould.cs
@@ -1,11 +1,22 @@
-﻿namespace MasterTDD.UnitTests.Services.Module6
+﻿using FluentAssertions;
+using MasterTDD.Services.Module6;
+
+namespace MasterTDD.UnitTests.Services.Module6
 {
     public class OddOrEvenDetectorServiceShould
     {
-        [Fact]
-        public void ReturnTrue()
+        private readonly OddOrEvenDetectorService _service;
+        public OddOrEvenDetectorServiceShould()
         {
+            _service = new OddOrEvenDetectorService();
+        }
 
+        [Fact]
+        public void ReturnTrueIfOdd()
+        {
+            var isOdd = _service.IsRandomNumberOdd();
+
+            isOdd.Should().BeTrue();
         }
     }
 }
