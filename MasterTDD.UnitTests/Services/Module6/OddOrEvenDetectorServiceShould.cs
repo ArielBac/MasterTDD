19722c7d7a020ec06e64e7aa0d14c8b9ac4faf5f
ArielBac <arielvieira65@gmail.com>
2025-05-01 08:46:49 -0400

    Addind CallGetRandomBetween1And100 test

--- MasterTDD.UnitTests/Services/Module6/OddOrEvenDetectorServiceShould.cs
+++ MasterTDD.UnitTests/Services/Module6/OddOrEvenDetectorServiceShould.cs
@@ -1,11 +1,13 @@
 ﻿using FluentAssertions;
 using MasterTDD.Services.Module6;
+using Moq;
 
 namespace MasterTDD.UnitTests.Services.Module6
 {
     public class OddOrEvenDetectorServiceShould
     {
         private readonly OddOrEvenDetectorService _service;
+        private readonly Mock<IRandomGenerator> _randomGeneratorMock = new();
         public OddOrEvenDetectorServiceShould()
         {
             _service = new OddOrEvenDetectorService();
@@ -17,6 +19,14 @@ namespace MasterTDD.UnitTests.Services.Module6
             var isOdd = _service.IsRandomNumberOdd();
 
             isOdd.Should().BeTrue();
+        }
+
+        [Fact]
+        public void CallGetRandomBetween1And100()
+        {
+            var isOdd = _service.IsRandomNumberOdd();
+
+            _randomGeneratorMock.Verify(x => x.GetRandomBetween1And100(), Times.Once);
         }
     }
 }
